dd4f6f9a530cad7d1d33b997cb80cd18a3b3f68a
mdainys <marius.dainys@pinenet.io>
2023-07-21 14:47:20 +0300

    LLP-65-BurgerMenu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import styles from "@/styles/BurgerMenu.module.scss";
+import { useRouter } from "next/router";
+import lt from "@/locales/lt";
+import en from "@/locales/en";
+import Image from "next/image";
 
 function BurgerMenu() {
+  const router = useRouter();
+  const { locale } = router;
+  const t = locale === "lt" ? lt : en;
+  const isBusiness = router.pathname.includes("/business");
+  const [langBtnState, setLangBtnState] = useState("ENG");
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef(null);
+  const [showInnerLinks, setShowInnerLinks] = useState({
+    payment: false,
+    credit: false,
+  });
+
+  const handleShowInnerLinks = (e) => {
+    const key = e.target.dataset.id;
+    setShowInnerLinks((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -15,6 +33,19 @@ function BurgerMenu() {
       setIsOpen(false);
     }
   };
+
+  useEffect(() => {
+    if (locale === "lt") {
+      setLangBtnState("ENG");
+    } else {
+      setLangBtnState("LT");
+    }
+  }, [locale]);
+
+  function setLanguage() {
+    if (langBtnState === "ENG") router.push("/", "/", { locale: "en" });
+    else router.push("/", "/", { locale: "lt" });
+  }
 
   useEffect(() => {
     document.addEventListener("mousedown", handleOutsideClick);
@@ -38,26 +69,209 @@ function BurgerMenu() {
         <span></span>
       </label>
       <ul className="menu">
-        <li>
-          <Link className={styles.burgerLink} href="/payments">
-            Payment
+        {isBusiness ? (
+          <Link className={styles.burgerLink} href="/">
+            {t.headerNavLinks.private}
           </Link>
-        </li>
-        <li>
-          <Link className={styles.burgerLink} href="/credits">
-            Credit
+        ) : (
+          <Link className={styles.burgerLink} href="/business">
+            {t.headerNavLinks.business}
           </Link>
-        </li>
-        <li>
-          <Link className={styles.burgerLink} href="deposits">
-            Deposit
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.burgerLink} href="/contacts">
-            Contact
-          </Link>
-        </li>
+        )}
+        {isBusiness ? (
+          <>
+            <li>
+              <Link className={styles.burgerLink} href="/business/deposit">
+                {t.headerNavLinks.deposit}
+              </Link>
+            </li>
+            <li>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  maxWidth: "270px",
+                }}
+              >
+                <p className={styles.burgerLink}>{t.headerNavLinks.payments}</p>
+                <Image
+                  src="/assets/images/chevronBlack.svg"
+                  width={14}
+                  height={8}
+                  alt="check"
+                  data-id="payment"
+                  onClick={(e) => handleShowInnerLinks(e)}
+                  className={showInnerLinks.payment ? "" : "rotate"}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
+              <div
+                className={
+                  showInnerLinks.payment ? "showInnerLink" : "hideInnerLink"
+                }
+              >
+                <Link
+                  className={styles.burgerLink}
+                  href="/business/current-account"
+                >
+                  {t.headerNavLinks.currentAccount}
+                </Link>
+                <Link className={styles.burgerLink} href="/business/payments">
+                  {t.headerNavLinks.payments}
+                </Link>
+              </div>
+            </li>
+            <li>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  maxWidth: "270px",
+                }}
+              >
+                <p className={styles.burgerLink}>{t.headerNavLinks.credit}</p>
+                <Image
+                  src="/assets/images/chevronBlack.svg"
+                  width={14}
+                  height={8}
+                  alt="check"
+                  data-id="credit"
+                  onClick={(e) => handleShowInnerLinks(e)}
+                  className={showInnerLinks.credit ? "" : "rotate"}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
+              <div
+                className={
+                  showInnerLinks.credit ? "showInnerLink" : "hideInnerLink"
+                }
+              >
+                <Link
+                  className={styles.burgerLink}
+                  href="/business/investment-loan"
+                >
+                  {t.headerNavLinks.investmentLoan}
+                </Link>
+                <Link
+                  className={styles.burgerLink}
+                  href="/business/capital-loan"
+                >
+                  {t.headerNavLinks.capitalLoan}
+                </Link>
+              </div>
+            </li>
+            <li>
+              <Link className={styles.burgerLink} href="/contacts">
+                {t.headerNavLinks.contacts}
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link className={styles.burgerLink} href="/deposit">
+                {t.headerNavLinks.deposit}
+              </Link>
+            </li>
+            <li>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  maxWidth: "270px",
+                }}
+              >
+                <p className={styles.burgerLink}>{t.headerNavLinks.payments}</p>
+                <Image
+                  src="/assets/images/chevronBlack.svg"
+                  width={14}
+                  height={8}
+                  alt="check"
+                  data-id="payment"
+                  onClick={(e) => handleShowInnerLinks(e)}
+                  className={showInnerLinks.payment ? "" : "rotate"}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
+              <div
+                className={
+                  showInnerLinks.payment ? "showInnerLink" : "hideInnerLink"
+                }
+              >
+                <Link className={styles.burgerLink} href="/product">
+                  {t.headerNavLinks.currentAccount}
+                </Link>
+                <Link className={styles.burgerLink} href="/payments">
+                  {t.headerNavLinks.payments}
+                </Link>
+              </div>
+            </li>
+            <li>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  maxWidth: "270px",
+                }}
+              >
+                <p className={styles.burgerLink}>{t.headerNavLinks.credit}</p>
+                <Image
+                  src="/assets/images/chevronBlack.svg"
+                  width={14}
+                  height={8}
+                  alt="check"
+                  data-id="credit"
+                  onClick={(e) => handleShowInnerLinks(e)}
+                  className={showInnerLinks.credit ? "" : "rotate"}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
+              <div
+                className={
+                  showInnerLinks.credit ? "showInnerLink" : "hideInnerLink"
+                }
+              >
+                <Link className={styles.burgerLink} href="/credits/mortgage">
+                  {t.headerNavLinks.mortgageLoan}
+                </Link>
+                <Link
+                  className={styles.burgerLink}
+                  href="/credits/consumer-loan"
+                >
+                  {t.headerNavLinks.consumerLoan}
+                </Link>
+                <Link className={styles.burgerLink} href="/credits/equity-loan">
+                  {t.headerNavLinks.equityLoan}
+                </Link>
+              </div>
+            </li>
+            <li>
+              <Link className={styles.burgerLink} href="/contacts">
+                {t.headerNavLinks.contacts}
+              </Link>
+            </li>
+          </>
+        )}
+        <p
+          style={{ width: "40px", fontWeight: "bold" }}
+          className="header-nav-link change-language-link"
+          onClick={setLanguage}
+          href="/eng"
+        >
+          {langBtnState}
+        </p>
       </ul>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,140 +2,199 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import lt from '@/locales/lt';
-import en from '@/locales/en';
+import lt from "@/locales/lt";
+import en from "@/locales/en";
 import BurgerMenu from "./BurgerMenu";
 
 function Header() {
-  const [langBtnState, setLangBtnState] = useState('ENG');
+  const [langBtnState, setLangBtnState] = useState("ENG");
 
   const router = useRouter();
-  const isBusiness = router.pathname.includes('/business')
+  const isBusiness = router.pathname.includes("/business");
 
   const { locale } = router;
-  const t = locale === 'lt' ? lt : en;
+  const t = locale === "lt" ? lt : en;
 
   const [paymentSubMenu, setPaymentSubMenu] = useState(false);
   const [creditSubMenu, setCreditSubMenu] = useState(false);
 
   const toggleSubMenu = (type, parentLink) => {
-    if (parentLink == 'payment') {
-      if (type === 'open') setPaymentSubMenu(true);
+    if (parentLink == "payment") {
+      if (type === "open") setPaymentSubMenu(true);
       else setPaymentSubMenu(false);
-      return
+      return;
     }
-    if (type === 'open') setCreditSubMenu(true);
+    if (type === "open") setCreditSubMenu(true);
     else setCreditSubMenu(false);
-  }
-  
-  useEffect(()=> {
-    if (locale === 'lt') {
-      setLangBtnState('ENG')
+  };
+
+  useEffect(() => {
+    if (locale === "lt") {
+      setLangBtnState("ENG");
     } else {
-      setLangBtnState('LT')
+      setLangBtnState("LT");
     }
-  }, [locale])
+  }, [locale]);
 
   function setLanguage() {
-    if (langBtnState === 'ENG') router.push('/', '/', {locale: 'en'})
-    else router.push('/', '/', {locale: 'lt'})
+    if (langBtnState === "ENG") router.push("/", "/", { locale: "en" });
+    else router.push("/", "/", { locale: "lt" });
   }
 
   return (
-      <header>
-        <div className="logo-block">
-          <Link href="/">
-            <Image
-              src="/assets/images/ltl-logo.svg"
-              alt="Picture of the author"
-              width={90}
-              height={46}
-            />
+    <header>
+      <div className="logo-block">
+        <Link href="/">
+          <Image
+            src="/assets/images/ltl-logo.svg"
+            alt="Picture of the author"
+            width={90}
+            height={46}
+          />
+        </Link>
+      </div>
+      <div className="bussiness-type-block">
+        {isBusiness ? (
+          <Link className="header-bussinness-type-nav-link" href="/">
+            {t.headerNavLinks.private}
           </Link>
-        </div>
-        <div className="bussiness-type-block">
-          {
-            isBusiness ? 
-            <Link className="header-bussinness-type-nav-link" href="/">{t.headerNavLinks.private}</Link>
-            :
-            <Link className="header-bussinness-type-nav-link" href="/business">{t.headerNavLinks.business}</Link>
-          }
-        </div>
-        <div className="menubar">
-          <nav className="header-navigation">
-            {
-              isBusiness ? 
-              <Link className="header-nav-link" href="/business/deposits">{ t.headerNavLinks.deposit }</Link>
-              :
-              <Link className="header-nav-link" href="/deposits">{ t.headerNavLinks.deposit }</Link>
-            }
-            <div className="navigation-parent-link" onMouseEnter={ () => {toggleSubMenu('open', 'payment')} } onMouseLeave={() => {toggleSubMenu('close', 'payment')}}>
-              
-              {t.headerNavLinks.payments}
-              
-              {
-                paymentSubMenu &&
-                <div className="dropDownMenu">
-                  <div className="toggleSubMenu">
-                  {
-                    isBusiness ? 
-                    <Link className="header-nav-link" href="/business/current-account">{t.headerNavLinks.currentAccount}</Link>
-                    :
-                    <Link className="header-nav-link" href="/product">{t.headerNavLinks.currentAccount}</Link>
-                  } 
-                  </div>
-                  <div className="toggleSubMenu">
-                    {
-                      isBusiness ? 
-                      <Link className="header-nav-link" href="/business/payments">{t.headerNavLinks.payments}</Link>
-                      :
-                      <Link className="header-nav-link" href="/payments">{t.headerNavLinks.payments}</Link>
-                    } 
-                  </div>
+        ) : (
+          <Link className="header-bussinness-type-nav-link" href="/business">
+            {t.headerNavLinks.business}
+          </Link>
+        )}
+      </div>
+      <div className="menubar">
+        <nav className="header-navigation">
+          {isBusiness ? (
+            <Link className="header-nav-link" href="/business/deposits">
+              {t.headerNavLinks.deposit}
+            </Link>
+          ) : (
+            <Link className="header-nav-link" href="/deposits">
+              {t.headerNavLinks.deposit}
+            </Link>
+          )}
+          <div
+            className="navigation-parent-link"
+            onMouseEnter={() => {
+              toggleSubMenu("open", "payment");
+            }}
+            onMouseLeave={() => {
+              toggleSubMenu("close", "payment");
+            }}
+          >
+            {t.headerNavLinks.payments}
+
+            {paymentSubMenu && (
+              <div className="dropDownMenu">
+                <div className="toggleSubMenu">
+                  {isBusiness ? (
+                    <Link
+                      className="header-nav-link"
+                      href="/business/current-account"
+                    >
+                      {t.headerNavLinks.currentAccount}
+                    </Link>
+                  ) : (
+                    <Link className="header-nav-link" href="/product">
+                      {t.headerNavLinks.currentAccount}
+                    </Link>
+                  )}
                 </div>
-              }
-            </div>
-            <div className="navigation-parent-link-credit" onMouseEnter={ () => {toggleSubMenu('open')} } onMouseLeave={() => {toggleSubMenu('close')}}>
-              { t.headerNavLinks.credit }
-              {
-                creditSubMenu &&
-                <div className="dropDownMenu">
-                  <div className="toggleSubMenu">
-                    {
-                      isBusiness ? 
-                      <Link className="header-nav-link" href="/business/investment-loan">{t.headerNavLinks.investmentLoan}</Link>
-                      :
-                      <Link className="header-nav-link" href="/credits/mortgage">{t.headerNavLinks.mortgageLoan}</Link>
-                    }
-                  </div>
-                  {
-                    !isBusiness && 
-                    <div className="toggleSubMenu">
-                      <Link className="header-nav-link" href="/credits/consumer-loan">{t.headerNavLinks.consumerLoan}</Link>
-                    </div>
-                  }
-                  <div className="toggleSubMenu">
-                    {
-                      isBusiness ? 
-                      <Link className="header-nav-link" href="/business/capital-loan">{t.headerNavLinks.capitalLoan}</Link>
-                      :
-                      <Link className="header-nav-link" href="/credits/equity-loan">{t.headerNavLinks.equityLoan}</Link>
-                    }
-                  </div>
+                <div className="toggleSubMenu">
+                  {isBusiness ? (
+                    <Link className="header-nav-link" href="/business/payments">
+                      {t.headerNavLinks.payments}
+                    </Link>
+                  ) : (
+                    <Link className="header-nav-link" href="/payments">
+                      {t.headerNavLinks.payments}
+                    </Link>
+                  )}
                 </div>
-              }
-            </div>
-            <Link className="header-nav-link" href="/contacts">{t.headerNavLinks.contacts}</Link>
-            <span className="header-nav-link change-language-link" onClick={setLanguage} href="/eng">{langBtnState}</span>
-          </nav>
-        </div>
-        <div className="header-action-buttons-block">
-          <Link className="header-action-button" href="/login">{ t.headerNavLinks.login }</Link>
-          <Link className="header-action-button" href="/open-account">{ t.headerNavLinks.openAccount }</Link>          
-        </div>
-        <BurgerMenu />
-      </header>
-  )
+              </div>
+            )}
+          </div>
+          <div
+            className="navigation-parent-link-credit"
+            onMouseEnter={() => {
+              toggleSubMenu("open");
+            }}
+            onMouseLeave={() => {
+              toggleSubMenu("close");
+            }}
+          >
+            {t.headerNavLinks.credit}
+            {creditSubMenu && (
+              <div className="dropDownMenu">
+                <div className="toggleSubMenu">
+                  {isBusiness ? (
+                    <Link
+                      className="header-nav-link"
+                      href="/business/investment-loan"
+                    >
+                      {t.headerNavLinks.investmentLoan}
+                    </Link>
+                  ) : (
+                    <Link className="header-nav-link" href="/credits/mortgage">
+                      {t.headerNavLinks.mortgageLoan}
+                    </Link>
+                  )}
+                </div>
+                {!isBusiness && (
+                  <div className="toggleSubMenu">
+                    <Link
+                      className="header-nav-link"
+                      href="/credits/consumer-loan"
+                    >
+                      {t.headerNavLinks.consumerLoan}
+                    </Link>
+                  </div>
+                )}
+                <div className="toggleSubMenu">
+                  {isBusiness ? (
+                    <Link
+                      className="header-nav-link"
+                      href="/business/capital-loan"
+                    >
+                      {t.headerNavLinks.capitalLoan}
+                    </Link>
+                  ) : (
+                    <Link
+                      className="header-nav-link"
+                      href="/credits/equity-loan"
+                    >
+                      {t.headerNavLinks.equityLoan}
+                    </Link>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+          <Link className="header-nav-link" href="/contacts">
+            {t.headerNavLinks.contacts}
+          </Link>
+          <span
+            className="header-nav-link change-language-link"
+            onClick={setLanguage}
+            href="/eng"
+          >
+            {langBtnState}
+          </span>
+        </nav>
+      </div>
+      <div className="header-action-buttons-block">
+        <Link className="header-action-button" href="/login">
+          {t.headerNavLinks.login}
+        </Link>
+        <Link className="header-action-button" href="/open-account">
+          {t.headerNavLinks.openAccount}
+        </Link>
+      </div>
+      <BurgerMenu />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
