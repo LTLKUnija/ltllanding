5b435c03b821fa71dcee5b9b4866aadf41e1a920
mdainys <marius.dainys@pinenet.io>
2023-10-04 12:03:11 +0300

    LLP-79- PP update

--- src/pages/api/data/privacyLinks.js
+++ src/pages/api/data/privacyLinks.js
@@ -1,50 +1,24 @@
 export const privacyLinks = [
-   {
-      linkName: "General provisions",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Identification and representation",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Banking secret and processing customer data",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Entry into agreements",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Exchange of information between bank and customer",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Information about accounts and transactions",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "General provisions",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Identification and representation",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Banking secret and processing customer data",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Entry into agreements",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Exchange of information between bank and customer",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-   {
-      linkName: "Information about accounts and transactions",
-      linkUrl: "https://www.handbook.fca.org.uk/handbook/GEN.pdf",
-   },
-]
+  {
+    linkName:
+      "Informacija apie asmens duomenų tvarkymą - juridinio asmens atstovas",
+    linkUrl:
+      "https://storage.cloud.google.com/ltlku_web_page/privacyAndPolicy/INFORMACIJA%20APIE%20ASMENS%20DUOMENU%CC%A8%20TVARKYMA%CC%A8_Fizinis%20asmuo%20(klientas).pdf",
+  },
+  {
+    linkName:
+      "Informacija apie asmens duomenų tvarkymą - fizinis asmuo (klientas)",
+    linkUrl:
+      "https://storage.cloud.google.com/ltlku_web_page/privacyAndPolicy/INFORMACIJA%20APIE%20ASMENS%20DUOMENU%CC%A8%20TVARKYMA%CC%A8_juridinio%20asmens%20atstovas%20(direktorius).pdf",
+  },
+  {
+    linkName: "Privatumo politika",
+    linkUrl:
+      "https://storage.cloud.google.com/ltlku_web_page/privacyAndPolicy/Privatumo%20politika.pdf",
+  },
+  {
+    linkName: "Slapukų politika",
+    linkUrl:
+      "https://storage.cloud.google.com/ltlku_web_page/privacyAndPolicy/Slapuku%CC%A8%20politika.pdf",
+  },
+];
