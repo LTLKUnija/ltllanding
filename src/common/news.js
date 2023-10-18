c8775bc67072e65f9761338fd264e2730eadb478
mdainys <marius.dainys@pinenet.io>
2023-10-18 11:22:30 +0300

    LLP-85-Importing Old News

---
+++ src/common/news.js
@@ -0,0 +1,70 @@
+const news = [
+  {
+    text: "Informuojame, kad 2023 m. vasario 22 d. įvykusiame LTL Kredito unijos pakartotiniame eiliniame visuotiniame narių susirinkime absoliučia balsų dauguma buvo pritarta Stebėtojų tarybos, Vidaus audito tarnybos, Valdybos ir Paskolų komiteto 2022 m. veiklos ataskaitoms. Buvo priimti nutarimai Dėl metinių finansinių ataskaitų rinkinio tvirtinimo, nutarimas Dėl 2022 m. pelno paskirstymo (nuostolių atlyginimo) tvarkos, taip pat nutarimas Dėl 2023 m. LTL Kredito unijos pajamų ir išlaidų sąmatos tvirtinimo. Balsų dauguma vietoj atsistatydinusio Stebėtojų tarybos nario buvo išrinktas naujas Stebėtojų tarybos narys. Priimtas nutarimas Dėl sutarties pratęsimo su audito įmone UAB ,,Verslo konsultacijos ir auditas“ atlikti 2022 m. Finansinių ataskaitų rinkinio auditą ir lėšų limito už suteiktą paslaugą nustatymo, taip pat priimtas nutarimas Dėl LTL Kredito unijos naujos redakcijos įstatų patvirtinimo.",
+    date: "2023/03/07",
+  },
+  {
+    text: "Pranešame, kad 2023 m. vasario 21 d. 16:00 val. (registracija prasideda 15:30 val.) LTL Kredito unijos patalpose, Sporto g. 18, Vilniuje, šaukiamas LTL Kredito unijos eilinis visuotinis narių susirinkimas, numatant šią darbotvarkę: Stebėtojų tarybos 2022 m. veiklos ataskaita. Ataskaitos įvertinimas. Vidaus audito tarnybos 2022 m. veiklos ataskaita. Ataskaitos įvertinimas. Valdybos 2022 m. veiklos ataskaita. Ataskaitos įvertinimas. Paskolų komiteto 2022 m. veiklos ataskaita. Ataskaitos įvertinimas. 2022 m. Metinių finansinių ataskaitų rinkinio su audito įmonės išvada pristatymas. Metinių finansinių ataskaitų rinkinio tvirtinimas. Pelno paskirstymo (nuostolių atlyginimo) tvarkos projekto pristatymas. Nutarimo dėl 2022 m. pelno paskirstymo (nuostolių atlyginimo) tvarkos priėmimas. Kredito unijos 2023 m. pajamų ir išlaidų sąmatos tvirtinimas. Sutarties su audito įmone ,,UAB verslo konsultacijos ir auditas“ atlikti 2022 m. metinių finansinių ataskaitų rinkinio auditą pratęsimas ir  4 356 eurų sumos už suteiktą paslaugą patvirtinimas. Kredito unijos stebėtojų tarybos nario atšaukimas. Kredito unijos stebėtojų tarybos nario rinkimas. Naujos redakcijos kredito unijos įstatų tvirtinimas. Norėdami susipažinti su darbotvarkės klausimų medžiaga ir/ar užpildyti Bendrąjį balsavimo biuletenį  prašome  nuo 2023 m. vasario 17 d kreiptis į LTL Kredito unijos Klientų aptarnavimo departamento vadovą Joną Toliušį, elektroniniu paštu: jonas.toliusis@ltlku.lt arba telefonu +370 67120771. Dėl nepakankamo kvorumo neįvykus susirinkimui, tos pačios darbotvarkės pakartotinis kredito unijos eilinis visuotinis narių susirinkimas bus kviečiamas 2023 m. vasario 22 d. 16:00 val. (registracija prasideda 15:30 val.) LTL Kredito unijos patalpose, Sporto g. 18, Vilniuje.",
+    date: "2023/01/30",
+  },
+  {
+    text: "Informuojame, kad 2022 m. gruodžio 28 d. vykusio LTL Kredito unijos pakartotinio neeilinio visuotinio narių susirinkimo metu iš anksto numatytais darbotvarkės klausimais absoliučia balsų dauguma buvo priimti šie sprendimai: Pritarta LTL Kredito unijos įstojimui į Jungtinę centrinę kredito uniją nuo 2023 m. sausio 1 d. ir įgaliojimų valdybai suteikimui suderinti įstojimo sąlygas bei įgaliojimų administracijos vadovui ir valdybos nariui suteikimui sudaryti su Jungtine centrine kredito unija narystės sutartį. Pritarta LTL Kredito unijos išstojimui iš Jungtinės centrinės kredito unijos ir įgaliojimų valdybai suteikimui suderinti išstojimo sąlygas bei užtikrinti jų vykdymą, įgyvendinant LTL Kredito unijos tikslą gauti specializuoto banko licenciją.",
+    date: "2022/12/01 ",
+  },
+  {
+    text: "Atnaujinsime dalį LTL Kredito unijos paslaugų įkainių Gerb. Kliente, Reguliariai peržiūrime savo teikiamų paslaugų įkainius ir nuo 2022 m. rugsėjo 7 d. Verslo klientams, o nuo 2022 m. spalio 7 d. Privatiems klientams atnaujinsime dalį paslaugų įkainių. Pagrindiniai pokyčiai yra susiję su lėšų likučio laikymu sąskaitoje, SEPA mokėjimais internetu, kreditų išmokėjimu, grąžinimu ir refinansavimu, garantijų suteikimu ir kt. Detalią informaciją apie aukščiau išvardintus ir kitus paslaugų įkainius bei jų pokyčius rasite čia: Privatiems Verslui. Jei sutinkate su pasikeitusiais paslaugų įkainiais, Jums nereikia imtis jokių veiksmų. Jei su pasikeitimais nesutinkate, turite teisę neatlygintinai nutraukti sąskaitos sutartį iki pakeitimų įsigaliojimo dienos. Naujiems klientams paslaugų įkainiai įsigalioja 2022 m. rugpjūčio 2 d. Dėkojame, kad naudojatės mūsų paslaugomis. Jūsų LTL Kredito unija",
+    date: "2022/08/03",
+  },
+  {
+    text: "Gerb. Kliente, Informuojame, kad atnaujinome Privatumo politiką ir pateikiame trumpą santrauką, kuri padės suprasti svarbiausius pokyčius: Patikslinome, kokiais tikslais renkame ir naudojame asmens duomenis bei atnaujinome duomenų gavėjų sąrašą; Papildėme Jūsų teisių, susijusių su asmens duomenų tvarkymu, aprašymą ir asmens duomenų kategorijų sąrašą; Politiką papildėme nuostatomis apie vaizdo stebėjimą ir tiesioginę rinkodarą; Patikslinome duomenų apsaugos pareigūno kontaktinę informaciją. Detalią informaciją apie aukščiau išvardintus ir kitus pokyčius rasite čia: Teisinė informacija. Jūsų LTL Kredito unija",
+    date: "2022/05/05",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+  {
+    text: "",
+    date: "",
+  },
+];
