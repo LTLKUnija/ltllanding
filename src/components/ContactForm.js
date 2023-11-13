dfb7597274d13d450e52eea73222c318c73b341b
mdainys <marius.dainys@pinenet.io>
2023-11-13 11:48:21 +0200

    LLP-112-Contacts-Form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,38 +1,53 @@
+import styles from "@/styles/ContactForm.module.scss";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import { useTranslation } from "next-i18next";
 
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-export const ContactForm = () => {
+export const ContactForm = ({ ...props }) => {
+  const { t } = useTranslation("common");
+
   const [state, handleSubmit] = useForm("xqkvjgol");
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+    return <p>{t("contactForm.joining")}</p>;
   }
+
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+    <div className={styles.formTitleBlock}>
+      {props.formTitle && (
+        <div>
+          <h3>{t("contactForm.title")}</h3>
+          <p>{t("contactForm.description")}</p>
+        </div>
+      )}
+      <form onSubmit={handleSubmit} className={styles.formBlock}>
+        <div className={styles.formInputBlock}>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder={t("contactForm.topicPlaceholder")}
+          />
+          <textarea
+            placeholder={t("contactForm.feedbackPlaceholder")}
+            className={styles.feedbackInput}
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <input
+            id="message"
+            name="message"
+            placeholder={t("contactForm.namePlaceholder")}
+          />
+          <input type="text" placeholder={t("contactForm.emailPlaceholder")} />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+        <div type="submit" disabled={state.submitting} className="containedBtn">
+          {t("contactForm.button")}
+        </div>
+      </form>
+    </div>
   );
-}
+};
