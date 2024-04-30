331d0c939164ab4fd93b5d721bf03b9e12be424a
Mrius Dainys <marius.dainys@pinetnet.io>
2024-04-30 13:48:26 +0300

    LLP-Google-Storage-Link-Changes

--- src/common/privacyLinks.js
+++ src/common/privacyLinks.js
@@ -2,21 +2,21 @@ export const privacyLinks = [
   {
     linkName: "processingPersonalData",
     linkUrl:
-      "https://storage.cloud.google.com/ltlku_web_page/privacyAndPolicy/INFORMACIJA%20APIE%20ASMENS%20DUOMENU%CC%A8%20TVARKYMA%CC%A8_Fizinis%20asmuo%20(klientas).pdf",
+      "https://storage.googleapis.com/ltlku_web_page/privacyAndPolicy/INFORMACIJA%20APIE%20ASMENS%20DUOMENU%CC%A8%20TVARKYMA%CC%A8_Fizinis%20asmuo%20(klientas).pdf",
   },
   {
     linkName: "processingPersonalDataClient",
     linkUrl:
-      "https://storage.cloud.google.com/ltlku_web_page/privacyAndPolicy/INFORMACIJA%20APIE%20ASMENS%20DUOMENU%CC%A8%20TVARKYMA%CC%A8_juridinio%20asmens%20atstovas%20(direktorius).pdf",
+      "https://storage.googleapis.com/ltlku_web_page/privacyAndPolicy/INFORMACIJA%20APIE%20ASMENS%20DUOMENU%CC%A8%20TVARKYMA%CC%A8_juridinio%20asmens%20atstovas%20(direktorius).pdf",
   },
   {
     linkName: "privacyPolicy",
     linkUrl:
-      "https://storage.cloud.google.com/ltlku_web_page/privacyAndPolicy/Privatumo%20politika.pdf",
+      "https://storage.googleapis.com/ltlku_web_page/privacyAndPolicy/Privatumo%20politika.pdf",
   },
   {
     linkName: "cookiePolicy",
     linkUrl:
-      "https://storage.cloud.google.com/ltlku_web_page/privacyAndPolicy/Slapuku%CC%A8%20politika.pdf",
+      "https://storage.googleapis.com/ltlku_web_page/privacyAndPolicy/Slapuku%CC%A8%20politika.pdf",
   },
 ];
--- src/common/tnclinks.js
+++ src/common/tnclinks.js
@@ -7,17 +7,17 @@ export const tncLinks = [
       {
         linkName: "generalCondition",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/accounts/Bendrosios%20salygos.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/accounts/Bendrosios%20salygos.pdf",
       },
       {
         linkName: "commisionInfo",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/accounts/Informacijos%20apie%20komisin%C4%AF%20atlyginim%C4%85%20dokumentas.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/accounts/Informacijos%20apie%20komisin%C4%AF%20atlyginim%C4%85%20dokumentas.pdf",
       },
       {
         linkName: "glossary",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/accounts/%C5%BDodynas.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/accounts/%C5%BDodynas.pdf",
       },
     ],
   },
@@ -29,12 +29,12 @@ export const tncLinks = [
       {
         linkName: "informationDepositor",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/temr-deposit/Informacijos%20ind%C4%97lininkui%20teikimo%20forma.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/temr-deposit/Informacijos%20ind%C4%97lininkui%20teikimo%20forma.pdf",
       },
       {
         linkName: "securitisedDeposit",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/temr-deposit/Terminuotojo%20ind%C4%97lio%20sutartis_Standartin%C4%97s%20s%C4%85lygos.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/temr-deposit/Terminuotojo%20ind%C4%97lio%20sutartis_Standartin%C4%97s%20s%C4%85lygos.pdf",
       },
     ],
   },
@@ -46,22 +46,22 @@ export const tncLinks = [
       {
         linkName: "currentAccountContract",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/payments/Einamosios%20saskaitos%20sutartis%20fiziniam%20asmeniui_Standartin%C4%97s%20s%C4%85lygos.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/payments/Einamosios%20saskaitos%20sutartis%20fiziniam%20asmeniui_Standartin%C4%97s%20s%C4%85lygos.pdf",
       },
       {
         linkName: "currentAccountAgreement",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/payments/Einamosios%20saskaitos%20sutartis%20juridiniam%20asmeniui_Standartin%C4%97s%20s%C4%85lygos.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/payments/Einamosios%20saskaitos%20sutartis%20juridiniam%20asmeniui_Standartin%C4%97s%20s%C4%85lygos.pdf",
       },
       {
         linkName: "yourRights",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/payments/J%C5%ABs%C5%B3%20teis%C4%97s%20vykdant%20mok%C4%97jimus%20Europoje.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/payments/J%C5%ABs%C5%B3%20teis%C4%97s%20vykdant%20mok%C4%97jimus%20Europoje.pdf",
       },
       {
         linkName: "paymentsServices",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/payments/Mok%C4%97jimo%20paslaug%C5%B3%20teikimo%20s%C4%85lygos.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/payments/Mok%C4%97jimo%20paslaug%C5%B3%20teikimo%20s%C4%85lygos.pdf",
       },
     ],
   },
@@ -73,7 +73,7 @@ export const tncLinks = [
       {
         linkName: "generalInfo",
         linkUrl:
-          "https://storage.cloud.google.com/ltlku_web_page/termsAndConditions/loans-to-private/BENDRO%20POB%C5%AAD%C5%BDIO%20INFORMACIJA%20APIE%20SU%20NEKILNOJAMUOJU%20TURTU%20SUSIJUS%C4%AE%20KREDIT%C4%84.pdf",
+          "https://storage.googleapis.com/ltlku_web_page/termsAndConditions/loans-to-private/BENDRO%20POB%C5%AAD%C5%BDIO%20INFORMACIJA%20APIE%20SU%20NEKILNOJAMUOJU%20TURTU%20SUSIJUS%C4%AE%20KREDIT%C4%84.pdf",
       },
     ],
   },
