7a7e2dc40c455953c82db2712ec2aa083ec7e054
Dmitrij Panasiuk <dmitrij.panasiuk@pinenet.io>
2024-06-21 13:34:59 +0300

    LTL - FORM CAPTCHA IMPLEMENTATION

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "@/styles/ContactForm.module.scss";
 import { useTranslation } from "next-i18next";
 import axios from "axios";
@@ -6,11 +6,7 @@ import axios from "axios";
 function ContactForm(props) {
   const { t } = useTranslation("common");
 
-  const [status, setStatus] = useState({
-    submitted: false,
-    submitting: false,
-    info: { error: false, msg: null },
-  });
+  const [finishedCaptcha, setFinishedCaptcha] = useState(false);
 
   const [inputs, setInputs] = useState({
     topic: "",
@@ -19,116 +15,113 @@ function ContactForm(props) {
     email: "",
   });
 
-  const handleServerResponse = (ok, msg) => {
-    if (ok) {
-      setStatus({
-        submitted: true,
-        submitting: false,
-        info: { error: false, msg: msg },
-      });
-      setInputs({
-        topic: "",
-        feedback: "",
-        name: "",
-        email: "",
-      });
-    } else {
-      setStatus({
-        info: { error: true, msg: msg },
-      });
-    }
-  };
-
   const handleOnChange = (e) => {
     e.persist();
     setInputs((prev) => ({
       ...prev,
       [e.target.id]: e.target.value,
     }));
-    setStatus({
-      submitted: false,
-      submitting: false,
-      info: { error: false, msg: null },
-    });
   };
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
-    axios({
-      method: "POST",
-      url: "https://submit-form.com/IOB5fxre0",
-      data: inputs,
-    })
-      .then((response) => {
-        handleServerResponse(
-          true,
-          "Thank you, your message has been submitted."
-        );
+    axios
+      .post("https://submit-form.com/IOB5fxre0", {
+        topic: inputs.topic,
+        feedback: inputs.feedback,
+        name: inputs.name,
+        email: inputs.email,
+
+        "g-recaptcha-response": grecaptcha.getResponse(),
       })
-      .catch((error) => {
-        handleServerResponse(false, error.response.data.error);
+      .then(function (response) {
+        window.location.reload();
+      })
+      .catch(function (response) {
+        console.error(response);
       });
   };
 
+  const captchaCompleted = () => {
+    setFinishedCaptcha(true);
+  };
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.google.com/recaptcha/api.js";
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    window.captchaCompleted = captchaCompleted;
+  }, []);
+
   return (
-    <div className={styles.formTitleBlock}>
-      {props?.formTitle && (
-        <div>
-          <h3>{t("contactForm.title")}</h3>
-          {/* <p>{t("contactForm.description")}</p> */}
-        </div>
-      )}
-      <form className={styles.formBlock} onSubmit={handleOnSubmit}>
-        <div className={styles.formInputBlock}>
-          <input
-            id="topic"
-            name="topic"
-            type="text"
-            onChange={handleOnChange}
-            required
-            placeholder={t("contactForm.topicPlaceholder")}
-            value={inputs.topic}
+    <div>
+      <div className={styles.formTitleBlock}>
+        {props?.formTitle && (
+          <div>
+            <h3>{t("contactForm.title")}</h3>
+            {/* <p>{t("contactForm.description")}</p> */}
+          </div>
+        )}
+        <form className={styles.formBlock} onSubmit={handleOnSubmit}>
+          <div className={styles.formInputBlock}>
+            <input
+              id="topic"
+              name="topic"
+              type="text"
+              onChange={handleOnChange}
+              required
+              placeholder={t("contactForm.topicPlaceholder")}
+              value={inputs.topic}
+            />
+            <textarea
+              id="feedback"
+              name="feedback"
+              required
+              type="text"
+              value={inputs.feedback}
+              placeholder={t("contactForm.feedbackPlaceholder")}
+              className={styles.feedbackInput}
+              onChange={handleOnChange}
+            />
+            <input
+              id="name"
+              name="name"
+              type="text"
+              required
+              value={inputs.name}
+              placeholder={t("contactForm.namePlaceholder")}
+              onChange={handleOnChange}
+            />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={inputs.email}
+              required
+              placeholder={t("contactForm.emailPlaceholder")}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div
+            style={{
+              marginBottom: "30px",
+            }}
+            className="g-recaptcha"
+            data-callback="captchaCompleted"
+            data-sitekey="6LfxFv4pAAAAAA0JxUq4Ho4nh7_9ipPgTUzsN2Bu"
           />
-          <textarea
-            id="feedback"
-            name="feedback"
-            type="text"
-            value={inputs.feedback}
-            placeholder={t("contactForm.feedbackPlaceholder")}
-            className={styles.feedbackInput}
-            onChange={handleOnChange}
-          />
-          <input
-            id="name"
-            name="name"
-            type="text"
-            value={inputs.name}
-            placeholder={t("contactForm.namePlaceholder")}
-            onChange={handleOnChange}
-          />
-          <input
-            id="email"
-            name="email"
-            type="email"
-            value={inputs.email}
-            placeholder={t("contactForm.emailPlaceholder")}
-            onChange={handleOnChange}
-          />
-        </div>
-        {/* <div className="containedBtn">{t("contactForm.button")}</div> */}
-        <button
-          className="containedBtn"
-          type="submit"
-          disabled={status.submitting}
-        >
-          {!status.submitting
-            ? !status.submitted
-              ? "Submit"
-              : "Submitted"
-            : "Submitting..."}
-        </button>
-      </form>
+          <button
+            className="containedBtn"
+            type="submit"
+            disabled={!finishedCaptcha}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
