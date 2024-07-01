72a9605589d8bbf1f49c0a76c013d859166d5665
Dmitrij Panasiuk <dmitrij.panasiuk@pinenet.io>
2024-07-01 13:00:47 +0300

    LLP - DEFAULT LT LOCALE

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,11 @@
-/** @type {import('next-i18next').UserConfig} */
 const path = require("path");
 
 module.exports = {
   react: { useSuspense: false },
   i18n: {
     defaultLocale: "lt",
-    locales: ["en", "lt"],
-    localePath: path.resolve("./public/locales"),
+    locales: ["lt", "en"],
+    localeDetection: false,
   },
+  localePath: path.resolve("./public/locales"),
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,26 @@
-import '@/styles/globals.scss';
+import "@/styles/globals.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../firebase.js";
-import { appWithTranslation } from 'next-i18next';
+import { appWithTranslation } from "next-i18next";
 import { Provider } from "react-redux";
 import { store } from "../store/store.js";
-
-
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const locale = router.locale || "lt";
+    document.cookie = `NEXT_LOCALE=${locale}; path=/`;
+  }, [router.locale]);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
-}
+  );
+};
 
-export default appWithTranslation(App)
+export default appWithTranslation(App);
