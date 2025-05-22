ebdcf285b83f531a4ad3ecd563b31d0ac4e35771
LTLKU <admin@ltlku.lt>
2025-05-22 09:09:27 +0300

    Interest rate change

--- src/common/Rates.js
+++ src/common/Rates.js
@@ -1,62 +1,19 @@
 export const businessRates = [
   {
     term: 1,
-    rate: 3,
+    rate: 2.05,
   },
   {
     term: 3,
-    rate: 3.4,
+    rate: 3.35,
   },
   {
     term: 6,
-    rate: 3.4,
+    rate: 3.35,
   },
   {
     term: 9,
-    rate: 3.4,
-  },
-  {
-    term: 12,
-    rate: 3.5,
-  },
-  {
-    term: 18,
-    rate: 3.5,
-  },
-  {
-    term: 24,
-    rate: 3.5,
-  },
-  {
-    term: 36,
-    rate: 3.5,
-  },
-  {
-    term: 48,
-    rate: 3.5,
-  },
-  {
-    term: 60,
-    rate: 3.5,
-  },
-];
-
-export const privateRates = [
-  {
-    term: 1,
-    rate: 2.5,
-  },
-  {
-    term: 3,
-    rate: 3.3,
-  },
-  {
-    term: 6,
-    rate: 3.3,
-  },
-  {
-    term: 9,
-    rate: 3.4,
+    rate: 3.35,
   },
   {
     term: 12,
@@ -64,22 +21,65 @@ export const privateRates = [
   },
   {
     term: 18,
-    rate: 3.5,
+    rate: 3.45,
   },
   {
     term: 24,
-    rate: 3.5,
+    rate: 3.45,
   },
   {
     term: 36,
-    rate: 3.55,
+    rate: 3.45,
   },
   {
     term: 48,
-    rate: 3.55,
+    rate: 3.45,
   },
   {
     term: 60,
-    rate: 3.55,
+    rate: 3.45,
+  },
+];
+
+export const privateRates = [
+  {
+    term: 1,
+    rate: 2,
+  },
+  {
+    term: 3,
+    rate: 2.90,
+  },
+  {
+    term: 6,
+    rate: 2.90,
+  },
+  {
+    term: 9,
+    rate: 2.90,
+  },
+  {
+    term: 12,
+    rate: 3,
+  },
+  {
+    term: 18,
+    rate: 3,
+  },
+  {
+    term: 24,
+    rate: 3,
+  },
+  {
+    term: 36,
+    rate: 2.95,
+  },
+  {
+    term: 48,
+    rate: 2.95,
+  },
+  {
+    term: 60,
+    rate: 2.95,
   },
 ];
