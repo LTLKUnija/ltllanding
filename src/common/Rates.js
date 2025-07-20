e3ec81e67ab74de1ee75eabf9722470951fbae4e
LTLKU <admin@ltlku.lt>
2025-07-20 21:46:25 +0300

    Interest rates updates

--- src/common/Rates.js
+++ src/common/Rates.js
@@ -9,35 +9,35 @@ export const businessRates = [
   },
   {
     term: 6,
-    rate: 3.05,
+    rate: 2.95,
   },
   {
     term: 9,
-    rate: 3.05,
+    rate: 2.95,
   },
   {
     term: 12,
-    rate: 3.25,
+    rate: 3.05,
   },
   {
     term: 18,
-    rate: 3.30,
+    rate: 2.95,
   },
   {
     term: 24,
-    rate: 3.30,
+    rate: 2.85,
   },
   {
     term: 36,
-    rate: 3.40,
+    rate: 2.85,
   },
   {
     term: 48,
-    rate: 3.40,
+    rate: 2.85,
   },
   {
     term: 60,
-    rate: 3.40,
+    rate: 2.85,
   },
 ];
 
@@ -48,38 +48,38 @@ export const privateRates = [
   },
   {
     term: 3,
-    rate: 2.70,
+    rate: 2.45,
   },
   {
     term: 6,
-    rate: 2.75,
+    rate: 2.45,
   },
   {
     term: 9,
-    rate: 2.75,
+    rate: 2.50,
   },
   {
     term: 12,
-    rate: 2.85,
+    rate: 2.55,
   },
   {
     term: 18,
-    rate: 2.80,
+    rate: 2.60,
   },
   {
     term: 24,
-    rate: 2.75,
+    rate: 2.60,
   },
   {
     term: 36,
-    rate: 2.75,
+    rate: 2.50,
   },
   {
     term: 48,
-    rate: 2.75,
+    rate: 2.50,
   },
   {
     term: 60,
-    rate: 2.75,
+    rate: 2.50,
   },
 ];
